a3f3ab4e4e89a4c0e9ad6c7a98ebd37746bf3a53
Your name here <vidabonney278@gmail.com>
2024-08-02 12:06:00 +0100

    ...

--- src/components/Weather/Modal.js
+++ src/components/Weather/Modal.js
@@ -4,7 +4,9 @@ import React from 'react';
 const Modal = ({ blog, closeModal }) => {
   return (
     <div className="fixed inset-0 z-20 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-lg w-full relative m-2 h-[500px] overflow-y-auto">
+      <div className="bg-center bg-cover p-2 md:p-8 lg:p-8 rounded-lg shadow-lg max-w-lg w-full relative m-2 h-[500px] overflow-y-auto"
+       style={{ backgroundImage: "url('/images/back1.jpeg')" }}>
+        
         <button
           onClick={closeModal}
           className="absolute top-2 right-2 text-red-500 text-2xl"
@@ -13,11 +15,13 @@ const Modal = ({ blog, closeModal }) => {
         </button>
         <h2 className="text-xl md:text-2xl lg:text-2xl font-bold text-blue-800 mb-4">{blog.title}</h2>
         <img src={blog.image} alt={blog.title} className="w-full h-64 object-cover rounded-lg mb-4" />
+        <div className='bg-white bg-opacity-30 p-2 rounded-lg'>
         <p className="text-blue-800 mb-4">{blog.description}</p>
         <div className="flex items-center mt-2">
           <img src={blog.author.image} alt={blog.author.name} className="w-8 h-8 rounded-full object-cover mr-2" />
           <span className="text-blue-500">{blog.author.name}</span>
         </div>
+      </div>
       </div>
     </div>
   );
