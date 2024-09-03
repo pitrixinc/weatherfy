0d8e2ed471359728a9fa38dacd873d361c2f5b5c
Your name here <vidabonney278@gmail.com>
2024-09-03 17:22:21 +0100

    ...

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -45,7 +45,7 @@ const Hero = () => {
                 <input
                   id="location"
                   type="text"
-                  className="px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+                  className="w-full px-5 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
                   placeholder="Enter Location"
                   value={location}
                   onChange={(e) => setLocation(e.target.value)}
