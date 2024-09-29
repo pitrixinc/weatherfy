bc8caba8410c79038ac0b1362c990e621af50935
Your name here <vidabonney278@gmail.com>
2024-09-29 08:04:32 +0100

    '...'

--- src/components/Weather/TheWeatherResult.js
+++ src/components/Weather/TheWeatherResult.js
@@ -540,7 +540,7 @@ useEffect(() => {
 
               {forecastData && (
             <div className=" shadow-md rounded-lg p-4">
-              <h2 className="text-2xl font-bold mb-4 text-center text-gray-700">7-Day Forecast</h2>
+              <h2 className="text-2xl font-bold mb-4 text-center text-gray-700">3-Day Forecast</h2>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {forecastData.map((day, index) => (
                   <div
